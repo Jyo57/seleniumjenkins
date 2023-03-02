3bb7873bf67049719cf70f7feb43b8cea80827d3
Jyo57 <rangalajyothi57@gmail.com>
2023-03-02 11:53:17 +0530

    My First Commit

---
+++ selenium_Jenkins_test/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\rangala.jyothi\OneDrive - Accenture\Desktop\Eclipse wrkspace\selenium_Jenkins_test
+mvn clean install
---
+++ selenium_Jenkins_test/src/test/java/test/Test1.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() {
+	  System.out.println("Tested Successfully");
+	  
+  }
+}
